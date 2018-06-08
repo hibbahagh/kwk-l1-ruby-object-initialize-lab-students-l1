5b34bb020ea9ad77d70be42d209d529080a697ca
hibbahagh <hibbahagh@gmail.com>
2018-06-08 20:35:01 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # dog.rb
+class Dog
+  
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed
+    @breed
+  end
+
+end
+
+dog = Dog.new("kaia", "Corgi")
+puts dog.name
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,16 @@
 # person.rb
+
+class Person
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+
+end
+
+person = Person.new("kaia")
+puts person.name
